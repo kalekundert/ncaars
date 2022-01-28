f5059feba8bbda83e40fedbd161b30a5f473988b
Kale Kundert <kale@thekunderts.net>
2022-01-28 15:17:38 -0500

    feat: sanitize chain names and residue indices
    Fixes #3

--- bin/copy_ncaa_into_scaffold.py
+++ bin/copy_ncaa_into_scaffold.py
@@ -15,11 +15,21 @@ import prody
 import docopt
 from scaffold import Scaffold
 
+def renumber_residues(atoms):
+    resi = atoms.getResindices() + 1
+    atoms.setResnums(resi)
+
 if __name__ == '__main__':
     args = docopt.docopt(__doc__)
     scaffold = Scaffold(args['<scaffold>'])
     ncaa = prody.parsePDB(args['<ncaa>'])
 
+    # Many protocols, including coupled moves, assume/require that the protein 
+    # is chain A and the ligand is chain X.
+    scaffold.apo_atoms.setChids('A')
+    ncaa.setChids('X')
+
     out = scaffold.apo_atoms + ncaa
+    renumber_residues(out)
     prody.writePDB(args['--output'], out)
 
