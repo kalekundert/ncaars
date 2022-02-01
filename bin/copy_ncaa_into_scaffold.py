e4e4d847727df6afcba6e0229f45de92c833ddca
Kale Kundert <kale@thekunderts.net>
2022-02-01 13:25:41 -0500

    feat: don't renumber residues
    Fixes #4

--- bin/copy_ncaa_into_scaffold.py
+++ bin/copy_ncaa_into_scaffold.py
@@ -15,10 +15,6 @@ import prody
 import docopt
 from scaffold import Scaffold
 
-def renumber_residues(atoms):
-    resi = atoms.getResindices() + 1
-    atoms.setResnums(resi)
-
 if __name__ == '__main__':
     args = docopt.docopt(__doc__)
     scaffold = Scaffold(args['<scaffold>'])
@@ -29,7 +25,12 @@ if __name__ == '__main__':
     scaffold.apo_atoms.setChids('A')
     ncaa.setChids('X')
 
+    # Give the ligand a defined residue number, so that it's easy to refer to 
+    # in rosetta scripts.  In contrast, don't renumber the protein.  The 
+    # scaffold includes config files (like resfiles) that may depend on the 
+    # original numbering.
+    ncaa.setResnums([1] * len(ncaa))
+
     out = scaffold.apo_atoms + ncaa
-    renumber_residues(out)
     prody.writePDB(args['--output'], out)
 
